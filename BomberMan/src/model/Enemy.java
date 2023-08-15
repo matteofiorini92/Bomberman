7deb587d6d30396a4e3d46d0fb4584dde40d926c
matteo <matteo.fiorini@rocketmail.com>
2023-08-15 22:06:15 +0100

    improved enemies movement - still changing direction midway

--- BomberMan/src/model/Enemy.java
+++ BomberMan/src/model/Enemy.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -22,22 +24,47 @@ public abstract class Enemy extends Character {
 		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 		Runnable moveTask = () -> {
 			boolean needsNewDirection = true;
-			Direction randomDirection = getRandomDirection(Direction.class);
+			Direction randomDirection = getRandomDirection(this, Direction.class);
 			needsNewDirection = !this.move(Board.getInstance(), randomDirection);
 			if (needsNewDirection) {				
-				randomDirection = getRandomDirection(Direction.class);
+				randomDirection = getRandomDirection(this, Direction.class);
 			}
 		};
-		executor.scheduleAtFixedRate(moveTask, 0, 375, TimeUnit.MILLISECONDS);
-
-		
+		executor.scheduleAtFixedRate(moveTask, 0, 500, TimeUnit.MILLISECONDS);
 	}
 	
-	public static Direction getRandomDirection(Class<Direction> d) {
-        Direction[] directions = d.getEnumConstants();
-        Random random = new Random();
-        int index = random.nextInt(directions.length);
-        return directions[index];
+	public static Direction getRandomDirection(Enemy e, Class<Direction> d) { // public?
+		Element[][] cells = Board.getInstance().getCells();
+		int[] currPosition = e.getPosition();
+		List<Direction> availableDirections = getAvailableDirections(currPosition, cells);
+		if (availableDirections.size() != 0) {			
+			Random random = new Random();
+			int index = random.nextInt(availableDirections.size());
+			return availableDirections.get(index);
+		}
+		
+		return Direction.RIGHT;
     }
+	
+	public static List<Direction> getAvailableDirections(int[] currPosition, Element[][] cells) { // public ?
+		List<Direction> availableDirection = new ArrayList<Direction>();
+		int coordinateX = currPosition[1];
+		int coordinateY = currPosition[0];
+		if (cells[coordinateY][coordinateX-1] instanceof EmptyTile) {
+			availableDirection.add(Direction.LEFT);
+		}
+		if (cells[coordinateY][coordinateX+1] instanceof EmptyTile) {
+			availableDirection.add(Direction.RIGHT);
+		}
+		if (cells[coordinateY-1][coordinateX] instanceof EmptyTile) {
+			availableDirection.add(Direction.UP);
+		}
+		if (cells[coordinateY+1][coordinateX] instanceof EmptyTile) {
+			availableDirection.add(Direction.DOWN);
+		}
+		
+		
+		return availableDirection;
+	}
 	
 }
