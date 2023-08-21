ff9fe2ceca04ef6bc5d14d6a9b0246b7176bea05
matteo <matteo.fiorini@rocketmail.com>
2023-08-21 15:48:41 +0100

    refactored character

--- BomberMan/src/view/Character.java
+++ BomberMan/src/view/Character.java
@@ -13,12 +13,16 @@ import model.Direction;
 import model.Position;
 
 public abstract class Character extends Element {
+	
+	public static final int CHARACTER_HEIGHT = 96;
+	public static final int CHARACTER_WIDTH = 64;
+	public static final int heightDifference = CHARACTER_HEIGHT - view.Item.ITEM_HEIGHT;
+
 	public static final long INVULNERABILITY_TIME = 3000;
-    public static final int CHARACTER_HEIGHT = 96;
-    public static final int CHARACTER_WIDTH = 64;
+	public static final int INVULNERABILITY_FRAMES = 20;
+	
 	public static long TIME_FOR_MOVEMENT = 375;
 	
-	private int heightDifference = CHARACTER_HEIGHT - view.Item.ITEM_HEIGHT;
 	
 	public static Map<Class<? extends model.Character>, String> prefixes = new HashMap<>();
 	static {
@@ -27,73 +31,75 @@ public abstract class Character extends Element {
 		prefixes.put(model.Bug.class, "bug");
 	}
 
-	private Direction direction;
 	private ImageView imageView;
 	
 	public Character(int[] position, Image im1)
 	{
-		this.direction = Direction.INITIAL;
 		imageView = new ImageView(im1);
 		
 		this.setLayoutY(position[0] * Item.ITEM_HEIGHT - heightDifference);
 		this.setLayoutX(position[1] * Item.ITEM_WIDTH);
 		
-        getChildren().add(getImageView());
-		
-//		imageView.setFitHeight(96);
-//		imageView.setFitWidth(64);
+        getChildren().add(imageView);
 	}
-
-	public Direction getDirection()	{ return direction;	}
-	public void setDirection(Direction direction) {	this.direction = direction;	}
 	
-	public ImageView getImageView() { return imageView; }
-	
+	public void update(Observable o, Object arg, Map<Direction, String> imageFiles) {
+		Object[] args = (Object[]) arg;
+		
+		if (args[0].equals(model.ChangeType.MOVE)) {			
+			move((model.Character)o, imageFiles, args[1]);
+		}
+		else if (args[0].equals(model.ChangeType.LOSE_LIFE)) {
+			loseLife();
+		}
+		else if (args[0].equals(model.ChangeType.DIE)) {
+			die();
+		}
+	}
 
 	public void move(model.Character character, Map<Direction, String> imageFiles, Object arg) {
 		
 		String prefix = prefixes.get(character.getClass());
 		Direction newDirection =  character.getDirection();
+
 		
-		if (newDirection.equals(Direction.INITIAL)) {
-			Image image = new Image(prefix + "-64x96/"+ imageFiles.get(newDirection) +".png");
-			imageView.setImage(image);
-		} else {
-			int[] newPosition = character.getPosition();
-			int[] prevPosition = (int[]) arg;
-			
-			String[] files = imageFiles.get(newDirection).split("\\s+");
-			
-			int xMove = newPosition[1] - prevPosition[1];
-			int yMove = newPosition[0] - prevPosition[0];
-			
-			Timeline timeline = new Timeline();
-			
-			for (int i = 0; i < files.length; i++) {
-				final int iPlusOne = i+1;
-				Image image = new Image(prefix + "-64x96/" + files[i]+ ".png");
-				KeyFrame keyFrame = new KeyFrame(Duration.millis(TIME_FOR_MOVEMENT/files.length * iPlusOne), event -> {
-			    	imageView.setImage(image);
-			    	this.setLayoutX(prevPosition[1] * Item.ITEM_WIDTH + xMove * Item.ITEM_WIDTH * iPlusOne / files.length);
-			    	this.setLayoutY(prevPosition[0] * Item.ITEM_HEIGHT + yMove * Item.ITEM_HEIGHT * iPlusOne / files.length - 32);
-			    });
-				timeline.getKeyFrames().add(keyFrame);
-			}
-			
-			timeline.play();
-		}	
+		int[] newPosition = character.getPosition();
+		int[] prevPosition = (int[]) arg;
+		
+		String[] files = imageFiles.get(newDirection).split("\\s+");
+		
+		int xMove = newPosition[1] - prevPosition[1];
+		int yMove = newPosition[0] - prevPosition[0];
+		
+		Timeline timeline = new Timeline();
+		
+		for (int frame = 0; frame < files.length; frame++) {
+			final int framePlusOne = frame+1;
+			Image image = new Image(prefix + "-64x96/" + files[frame]+ ".png");
+			KeyFrame keyFrame = new KeyFrame(Duration.millis(TIME_FOR_MOVEMENT/files.length * framePlusOne), event -> {
+		    	imageView.setImage(image);
+		    	this.setLayoutX(prevPosition[1] * Item.ITEM_WIDTH + xMove * Item.ITEM_WIDTH * framePlusOne / files.length);
+		    	this.setLayoutY(prevPosition[0] * Item.ITEM_HEIGHT + yMove * Item.ITEM_HEIGHT * framePlusOne / files.length - 32);
+		    });
+			timeline.getKeyFrames().add(keyFrame);
+		}
+		
+		timeline.play();
+	
 	}
+	
+	
+	/**
+	 * Flashing animation for when a character loses a life without dying
+	 */
 	
 	public void loseLife() {
 		Timeline timeline = new Timeline();
 		
-		for (int frame = 0; frame < 10; frame++) {
+		for (int frame = 0; frame < INVULNERABILITY_FRAMES; frame++) {
 			final int framePlusOne = frame+1;
-			KeyFrame keyFrame = new KeyFrame(Duration.millis(INVULNERABILITY_TIME/10 * framePlusOne), event -> {
-
-				
-				imageView.setVisible(framePlusOne % 2 == 0);	
-				
+			KeyFrame keyFrame = new KeyFrame(Duration.millis(INVULNERABILITY_TIME/INVULNERABILITY_FRAMES * framePlusOne), event -> {
+				imageView.setVisible(framePlusOne % 2 == 0);				
 			});
 			timeline.getKeyFrames().add(keyFrame);
 		}
@@ -106,20 +112,6 @@ public abstract class Character extends Element {
 	}
 	
 	
-	public void update(Observable o, Object arg, Map<Direction, String> imageFiles) {
-		Object[] args = (Object[]) arg;
-
-		if (args[0].equals(model.ChangeType.MOVE)) {			
-			move((model.Character)o, imageFiles, args[1]);
-		}
-		else if (args[0].equals(model.ChangeType.LOSE_LIFE)) {
-			//flash();
-			loseLife();
-		}
-		else if (args[0].equals(model.ChangeType.DIE)) {
-			die();
-		}
-	}
 	
 	
 	
